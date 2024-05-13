2196b294243ea02db9fdcf02b3c526608fa0dc63
Christianmagni97 <christianmagni28@gmail.com>
2024-05-13 11:51:49 +0200

    esericizio base

---
+++ script.js
@@ -0,0 +1,5 @@
+/*L'utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.*/ 
+const userNumber = prompt('Chose one number');
+const userNumber = prompt('Chose one number');
+console. log(userNumber);
