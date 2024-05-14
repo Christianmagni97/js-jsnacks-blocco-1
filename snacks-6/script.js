be693bca47ad03ccdb3c7862b7b673b2e2bc9b03
Christianmagni97 <christianmagni28@gmail.com>
2024-05-14 10:57:09 +0200

    snack 6

---
+++ snacks-6/script.js
@@ -0,0 +1,5 @@
+const number = [1,2,3,4,5,6,7,8,9,10.101,102,103,104,105,106,107,108,109,110];
+for (let index = 0 ; index < SVGAnimatedNumberList.length; index++){
+    if (index % 2 == 1)
+    
+}    
